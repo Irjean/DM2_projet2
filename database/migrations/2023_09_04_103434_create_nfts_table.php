75743aef67fcd04d8487476ffea05da6690491df
Irjean <c.spileers@outlook.fr>
2023-09-04 17:52:14 +0200

    init

---
+++ database/migrations/2023_09_04_103434_create_nfts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('nfts', function (Blueprint $table) {
+            $table->id();
+            $table->string("title", 45);
+            $table->string("artist", 45);
+            $table->string("description", 255);
+            $table->string("adress", 255);
+            $table->string("standard_token", 10);
+            $table->float("price");
+            $table->string("image", 255);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('nfts');
+    }
+};
